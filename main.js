891b291e26ac5f1d5ddb75335fe0c0bc25f8e25d
alix103 <atbermudez96@misena.edu.co>
2022-10-25 11:41:17 -0500

    Introducción a JavaScript

---
+++ main.js
@@ -0,0 +1,17 @@
+// variables
+
+// nombre = "Alix";
+// let num = 10;
+// const num2 = 10; 
+
+// console.log(nombre + ' ' + num + ' ' + num2);
+
+// console.log(`${nombre} ${num} ${num2}` );
+
+// sumar dos numeros
+
+var n1 = 10;
+var n2 = 20;
+var suma = n1 + n2;
+
+console.log(suma);
